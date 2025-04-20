e41f1264b2c108879316aa6bca5f07f521163341
Kumar P <161574943+kumar7505@users.noreply.github.com>
2025-04-20 19:16:03 +0530

    setup

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../Components/layout/Layout'
+
+const Home = () => {
+  return (
+    <Layout>
+        Home
+    </Layout>
+  )
+}
+
+export default Home
