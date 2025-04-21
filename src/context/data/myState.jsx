2f3c5b8650c681c3296f61a044a568ffeda4f46f
Kumar P <161574943+kumar7505@users.noreply.github.com>
2025-04-21 08:55:40 +0530

    navbar

---
+++ src/context/data/myState.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import myContext from './myContext'
+
+const MyState = (props) => {
+    const [mode, setMode] = useState("light")
+
+    const toggleMode = () => {
+      if(mode === "light"){
+        setMode("dark");
+        document.body.style.backgroundColor = "rgb(17, 24, 39)"
+      } else {
+        setMode("light");
+        document.body.style.backgroundColor = "#fff";
+      }
+    }
+  return (
+    <myContext.Provider
+        value={{
+          mode,
+          toggleMode,
+        }}
+    >
+        {props.children}
+    </myContext.Provider>
+  )
+}
+
+export default MyState;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Layout from '../../Components/layout/Layout'
+import myContext from '../../context/data/myContext'
 
 const Home = () => {
+    const context = useContext(myContext);
+    console.log(context);
+    const {name} = context;
   return (
     <Layout>
         Home
