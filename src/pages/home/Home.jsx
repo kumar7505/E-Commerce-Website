6e2e07b9b22acc6e71d49e9793d2c26195ba6ac0
Kumar P <161574943+kumar7505@users.noreply.github.com>
2025-04-21 09:14:21 +0530

    home page UI

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,11 @@
 import React, { useContext } from 'react'
 import Layout from '../../Components/layout/Layout'
 import myContext from '../../context/data/myContext'
+import HeroSection from '../../Components/herosection/HeroSection';
+import Filter from '../../Components/filter/Filter';
+import ProductCard from '../../Components/productCard/ProductCard';
+import Track from '../../Components/track/Track';
+import Testimonial from '../../Components/testimonial/Testimonial';
 
 const Home = () => {
     const context = useContext(myContext);
@@ -8,7 +13,11 @@ const Home = () => {
     const {name} = context;
   return (
     <Layout>
-        Home
+      <HeroSection />
+      <Filter />
+      <ProductCard />
+      <Track />
+      <Testimonial />
     </Layout>
   )
 }
