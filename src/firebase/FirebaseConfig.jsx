6915a426d2ee683bf0da357ce1aa916d36b7a11c
Kumar P <161574943+kumar7505@users.noreply.github.com>
2025-04-24 15:57:42 +0530

    Firebase and Redux setup

---
+++ src/firebase/FirebaseConfig.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const fireDB = getFirestore(app);
+const auth = getAuth(app)
+export {fireDB,auth } ;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,24 @@
 import React, { useContext } from 'react'
 import Layout from '../../Components/layout/Layout'
-import myContext from '../../context/data/myContext'
 import HeroSection from '../../Components/herosection/HeroSection';
 import Filter from '../../Components/filter/Filter';
 import ProductCard from '../../Components/productCard/ProductCard';
 import Track from '../../Components/track/Track';
 import Testimonial from '../../Components/testimonial/Testimonial';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart, deleteFromCart } from '../../redux/cartSlice';
 
 const Home = () => {
-    const context = useContext(myContext);
-    console.log(context);
-    const {name} = context;
+  const dispatch = useDispatch();
+  const cartItem = useSelector((state) => state.cart);
+
+  const addCart = () => {
+    dispatch(addToCart("shirt"))
+  }
+  const deleteCart = () => {
+    dispatch(deleteFromCart("shirt"))
+  }
+
   return (
     <Layout>
       <HeroSection />
